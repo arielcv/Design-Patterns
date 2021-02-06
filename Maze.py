cac62ccf4de68397a78d6d68b8916a9f7625f96a
arielcv <arielcv96@nauta.cu>
2021-02-05 23:24:19 -0500

    Create the enum directions

--- Maze.py
+++ Maze.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
 class MapSite():
     def Enter(self):
         raise NotImplementedError("Abstract Base Class method")
+
+class Directions(Enum):
+    North = 0
+    East = 1
+    South = 2
+    West = 3
+
